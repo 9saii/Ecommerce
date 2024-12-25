8f16e8cf7270eb70a757108986f98a039a0e1794
CQSTechSolutions <cqstechsolutionsofficial@gmail.com>
2024-12-25 13:32:51 +0530

    bug fixes

--- server/server.js
+++ server/server.js
@@ -31,7 +31,6 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ 
         error: 'Something went wrong!',
-        message: process.env.NODE_ENV === 'development' ? err.message : undefined
     });
 });
 
